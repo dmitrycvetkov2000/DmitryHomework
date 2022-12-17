a0b1758aea4a47a2b907237e2b219f43694e0d5e
dmitrycvetkov2000 <dimacvetkov2000@mail.ru>
2022-12-17 21:32:40 +0300

    Initial Commit

---
+++ DmitryHomework/DmitryHomework.h
@@ -0,0 +1,18 @@
+//
+//  DmitryHomework.h
+//  DmitryHomework
+//
+//  Created by Дмитрий Цветков on 17.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DmitryHomework.
+FOUNDATION_EXPORT double DmitryHomeworkVersionNumber;
+
+//! Project version string for DmitryHomework.
+FOUNDATION_EXPORT const unsigned char DmitryHomeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DmitryHomework/PublicHeader.h>
+
+
